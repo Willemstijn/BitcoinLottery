de770cc558e8da8185d754e2221772795d6b2aaa
Bas Willemstijn <Bas.Willemstijn@mn.nl>
2020-06-07 17:18:50 +0200

    Minor improvements to script

--- run_lottery.py
+++ run_lottery.py
@@ -1,12 +1,14 @@
-#!/usr/bin/python
+#!/usr/bin/env python
+
 from subprocess import Popen
 import sys
 import logging
+import time
 from datetime import datetime
 
 # Use sys.argv[1] if you want to use this file like this "run_lottery.py <filename>"
 # filename = sys.argv[1]
-filename = 'bitcoinLottery.py'
+filename = 'checkBtcAddress.py'
 
 logging.basicConfig(filename='report.log',level=logging.DEBUG)
 
@@ -17,3 +19,4 @@ while True:
     message = 'Started Bitcoin Lottery: '
     logging.info(message + sttime)
     p.wait()
+    time.sleep(5)
